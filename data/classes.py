23e569abf58b8861a7a7a510b1e49481c025f9c7
Kevin <kevin.tranr@outlook.com>
2022-11-22 23:49:53 -0500

    Learning about classes and instantiating

--- data/classes.py
+++ data/classes.py
@@ -1,3 +1,18 @@
 class Player:
+    def __init__(self, name, pts, pf, tov, blk, stl, ast, rb):
+        self.name = name
+        self.pts = pts
+        self.pf = pf
+        self.tov = tov
+        self.blk = blk
+        self.stl = stl
+        self.ast = ast
+        self.rb = rb
+    def get_name(self):
+        return self.name
+class Team:
     def __init__(self, name):
         self.name = name
+        self.players = []
+    def add_player(self, player):
+        self.players.append(player)
--- data/reading_writing.py
+++ data/reading_writing.py
@@ -1,10 +1,21 @@
 # https://www.youtube.com/watch?v=Uh2ebFW8OYM --> Reading and writing to files
 
+import csv
+import classes
+
 # open a file for reading. r indicates reading, w writing and r+ reading and writing
 # remember to close file after finished with what you are doing otherwise can cause leaks
 # this with block allows us to work with this file within this block of code and automatically
 # close it for us once we are finished
 with open(r'data\players.txt', 'r') as f:
-    for line in f:
-        print(line, end='')
+    players = csv.reader(f, delimiter=',')
+    for p in players:
+    	print(p)
+        #create a player object
+player = classes.Player(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7])
+print(player.get_name())
+        
+
+
+
 
